adcde79b720b81db05c4dd9b456f4e2fb3dc28ec
daniaki <7043686+daniaki@users.noreply.github.com>
2023-01-10 13:51:13 +1100

    Reducers for custom flag actions

---
+++ assets/redux/reducers/customFlagReducer.js
@@ -0,0 +1,20 @@
+import { actionTypes } from "../actions/customFlagActions";
+
+const customFlagActions = (state, action) => {
+  if (state === undefined) {
+    return [];
+  }
+
+  switch (action.type) {
+    case actionTypes.SET_CUSTOM_FLAGS:
+      return action.flags;
+    case actionTypes.SET_CUSTOM_FlAG:
+      return [...state.filter(f => f.id !== action.flag.id), action.flag];
+    case actionTypes.UNSET_CUSTOM_FLAG:
+      return state.filter(f => f.id !== action.flag.id);
+    default:
+      return state;
+  }
+};
+
+export default customFlagActions;
