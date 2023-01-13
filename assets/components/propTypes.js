c455f366717ab09d0b8ae374ed2d5b80344b23ad
daniaki <7043686+daniaki@users.noreply.github.com>
2023-01-13 12:00:41 +1100

    Moved prop type definition to prop types file

--- assets/components/propTypes.js
+++ assets/components/propTypes.js
@@ -11,3 +11,10 @@ export const CurationAssignmentPropType = PropTypes.shape({
   result: CurationResultPropType,
   variant: PropTypes.object, // eslint-disable-line react/forbid-prop-types
 });
+
+export const CustomFlagPropType = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  key: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  shortcut: PropTypes.string.isRequired,
+});
