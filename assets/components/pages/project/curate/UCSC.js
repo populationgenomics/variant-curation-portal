a013a95a99dce1a202b6a18af20892a048fb03ec
daniaki <7043686+daniaki@users.noreply.github.com>
2023-04-27 13:55:09 +1000

    Set correct session and id depending on grch ref

--- assets/components/pages/project/curate/UCSC.js
+++ assets/components/pages/project/curate/UCSC.js
@@ -10,6 +10,12 @@ import { Header, Segment } from "semantic-ui-react";
 
 export const UCSCVariantView = ({ settings, variant }) => {
   const assembly = variant.reference_genome === "GRCh38" ? "hg38" : "hg19";
+  const id = `ucsc-variant-${variant.reference_genome.toLowerCase()}`;
+  const title = `UCSC variant view (${variant.reference_genome})`;
+  const sessionName =
+    variant.reference_genome === "GRCh38"
+      ? settings.ucsc_session_name_grch38
+      : settings.ucsc_session_name_grch37;
 
   let url = `https://genome.ucsc.edu/cgi-bin/hgTracks?db=${assembly}&position=${encodeURIComponent(
     `chr${variant.chrom}:${variant.pos - 25}-${variant.pos + 25}`
@@ -17,33 +23,27 @@ export const UCSCVariantView = ({ settings, variant }) => {
     `${assembly}.chr${variant.chrom}:${variant.pos}-${variant.pos}`
   )}`;
 
-  if (settings.ucsc_username && settings.ucsc_session_name) {
-    url = `${url}&hgS_doOtherUser=submit&hgS_otherUserName=${settings.ucsc_username}&hgS_otherUserSessionName=${settings.ucsc_session_name}`;
+  if (settings.ucsc_username && sessionName) {
+    url = `${url}&hgS_doOtherUser=submit&hgS_otherUserName=${settings.ucsc_username}&hgS_otherUserSessionName=${sessionName}`;
   }
 
   if (process.env.NODE_ENV === "development") {
     return (
-      <Segment placeholder id="ucsc" textAlign="center">
-        <p>UCSC variant view</p>
+      <Segment placeholder id={id} textAlign="center">
+        <p>{title}</p>
         <a href={url}>{url}</a>
       </Segment>
     );
   }
 
-  return (
-    <iframe
-      title="UCSC variant view"
-      id="ucsc"
-      src={url}
-      style={{ width: "100%", height: "4000px" }}
-    />
-  );
+  return <iframe title={title} id={id} src={url} style={{ width: "100%", height: "4000px" }} />;
 };
 
 UCSCVariantView.propTypes = {
   settings: PropTypes.shape({
     ucsc_username: PropTypes.string,
-    ucsc_session_name: PropTypes.string,
+    ucsc_session_name_grch37: PropTypes.string,
+    ucsc_session_name_grch38: PropTypes.string,
   }).isRequired,
   variant: PropTypes.shape({
     chrom: PropTypes.string.isRequired,
@@ -54,6 +54,12 @@ UCSCVariantView.propTypes = {
 
 export const UCSCGeneView = ({ settings, variant }) => {
   const hasAnnotations = variant.annotations.length > 0;
+  const id = `ucsc-gene-${variant.reference_genome.toLowerCase()}`;
+  const title = `UCSC gene view (${variant.reference_genome})`;
+  const sessionName =
+    variant.reference_genome === "GRCh38"
+      ? settings.ucsc_session_name_grch38
+      : settings.ucsc_session_name_grch37;
 
   if (!hasAnnotations) {
     return (
@@ -78,33 +84,27 @@ export const UCSCGeneView = ({ settings, variant }) => {
     `${assembly}.chr${variant.chrom}:${variant.pos}-${variant.pos}`
   )}`;
 
-  if (settings.ucsc_username && settings.ucsc_session_name) {
-    url = `${url}&hgS_doOtherUser=submit&hgS_otherUserName=${settings.ucsc_username}&hgS_otherUserSessionName=${settings.ucsc_session_name}`;
+  if (settings.ucsc_username && sessionName) {
+    url = `${url}&hgS_doOtherUser=submit&hgS_otherUserName=${settings.ucsc_username}&hgS_otherUserSessionName=${sessionName}`;
   }
 
   if (process.env.NODE_ENV === "development") {
     return (
-      <Segment placeholder id="ucsc-gene" textAlign="center">
-        <p>UCSC gene view</p>
+      <Segment placeholder id={id} textAlign="center">
+        <p>{title}</p>
         <a href={url}>{url}</a>
       </Segment>
     );
   }
 
-  return (
-    <iframe
-      title="UCSC gene view"
-      id="ucsc-gene"
-      src={url}
-      style={{ width: "100%", height: "4000px" }}
-    />
-  );
+  return <iframe title={title} id={id} src={url} style={{ width: "100%", height: "4000px" }} />;
 };
 
 UCSCGeneView.propTypes = {
   settings: PropTypes.shape({
     ucsc_username: PropTypes.string,
-    ucsc_session_name: PropTypes.string,
+    ucsc_session_name_grch37: PropTypes.string,
+    ucsc_session_name_grch38: PropTypes.string,
   }).isRequired,
   variant: PropTypes.shape({
     annotations: PropTypes.arrayOf(
