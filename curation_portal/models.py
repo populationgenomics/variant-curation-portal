ab05d831ba7a251d845c8ff15bd4dc7e9e223055
Daniel <iamdanielreti@gmail.com>
2023-07-26 13:30:48 +1000

    updated modles

--- curation_portal/models.py
+++ curation_portal/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.db.models.signals import post_delete, pre_save, post_save
 from django.dispatch.dispatcher import receiver
@@ -44,6 +45,7 @@ class Variant(models.Model):
         max_length=6, choices=[("GRCh37", "GRCh37"), ("GRCh38", "GRCh38")], default="GRCh37"
     )
     variant_id = models.CharField(max_length=1000)
+    sample_ids = ArrayField(models.CharField(max_length=1000, blank=True), null=True, blank=True)
     liftover_variant_id = models.CharField(max_length=1000, null=True, blank=True)
     chrom = models.CharField(max_length=2)
     pos = models.IntegerField()
@@ -57,6 +59,9 @@ class Variant(models.Model):
     AF = models.FloatField(null=True, blank=True)
     n_homozygotes = models.IntegerField(null=True, blank=True)
     n_heterozygotes = models.IntegerField(null=True, blank=True)
+    GT = ArrayField(models.CharField(max_length=1000, blank=True), null=True, blank=True)
+    DP = ArrayField(models.IntegerField(null=True, blank=True), null=True, blank=True)
+    GQ = ArrayField(models.IntegerField(null=True, blank=True), null=True, blank=True)
 
     class Meta:
         db_table = "curation_variant"
