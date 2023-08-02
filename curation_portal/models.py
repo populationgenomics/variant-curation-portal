6ef34b55fdc126b4ff13b6079bf69d4c1ecf09b8
Daniel Esposito <7043686+daniaki@users.noreply.github.com>
2023-08-02 14:37:35 +1000

    Update models.py
    Suggestion to disallow `null` in the `ArrayField` base field, in line with the suggested migration changes.

--- curation_portal/models.py
+++ curation_portal/models.py
@@ -45,7 +45,11 @@ class Variant(models.Model):
         max_length=6, choices=[("GRCh37", "GRCh37"), ("GRCh38", "GRCh38")], default="GRCh37"
     )
     variant_id = models.CharField(max_length=1000)
-    sample_ids = ArrayField(models.CharField(max_length=1000, blank=True), null=True, blank=True)
+    sample_ids = ArrayField(
+        models.CharField(max_length=1000, blank=True, null=False),
+        null=True,
+        blank=True,
+    )
     liftover_variant_id = models.CharField(max_length=1000, null=True, blank=True)
     chrom = models.CharField(max_length=2)
     pos = models.IntegerField()
@@ -59,9 +63,21 @@ class Variant(models.Model):
     AF = models.FloatField(null=True, blank=True)
     n_homozygotes = models.IntegerField(null=True, blank=True)
     n_heterozygotes = models.IntegerField(null=True, blank=True)
-    GT = ArrayField(models.CharField(max_length=1000, blank=True), null=True, blank=True)
-    DP = ArrayField(models.IntegerField(null=True, blank=True), null=True, blank=True)
-    GQ = ArrayField(models.IntegerField(null=True, blank=True), null=True, blank=True)
+    GT = ArrayField(
+        models.CharField(max_length=1000, blank=True, null=False),
+        null=True,
+        blank=True,
+    ),
+    DP = ArrayField(
+        models.IntegerField(null=True, blank=True, null=False),
+        null=True,
+        blank=True,
+    )
+    GQ = ArrayField(
+        models.IntegerField(null=True, blank=True, null=False),
+        null=True,
+        blank=True,
+    )
 
     class Meta:
         db_table = "curation_variant"
