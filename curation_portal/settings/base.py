abd8c0d9d1558052d59efb8197df573338e4edb5
Daniel Esposito <7043686+daniaki@users.noreply.github.com>
2023-08-22 09:39:52 +1000

    set consistent defaults for db settings

--- curation_portal/settings/base.py
+++ curation_portal/settings/base.py
@@ -84,10 +84,10 @@ WSGI_APPLICATION = "curation_portal.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.getenv("DB_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.getenv("DB_USER", ""),
-        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.getenv("DB_DATABASE", "curator-dev"),
+        "USER": os.getenv("DB_USER", "curator-dev"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "password"),
         "HOST": os.getenv("DB_HOST", "localhost"),
         "PORT": os.getenv("DB_PORT", "5432"),
     }
