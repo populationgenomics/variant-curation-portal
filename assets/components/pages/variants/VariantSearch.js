1ff141915024722d00bc9d3352834171438c7610
Daniel Esposito <7043686+daniaki@users.noreply.github.com>
2023-08-30 10:37:15 +1000

    Resolve GRCh37 from CA Reg

--- assets/components/pages/variants/VariantSearch.js
+++ assets/components/pages/variants/VariantSearch.js
@@ -1,6 +1,5 @@
 import PropTypes from "prop-types";
 import React, { useState } from "react";
-import { withRouter } from "react-router-dom";
 import { Button, Form, Header, Message, Segment } from "semantic-ui-react";
 
 const VARIANT_ID_REGEX = /^(\d+|X|Y)[-:]([0-9]+)[-:]([ACGT]+)[-:]([ACGT]+)$/;
@@ -28,6 +27,15 @@ const VariantIdForm = ({ onSubmit }) => {
       <Button disabled={!variantId || !isValid} type="submit">
         Search
       </Button>
+      <Button
+        type="reset"
+        onClick={() => {
+          setVariantId("");
+          onSubmit(null);
+        }}
+      >
+        Clear
+      </Button>
     </Form>
   );
 };
@@ -36,8 +44,8 @@ VariantIdForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
 
-const fetchVariantIdFromClingenRegistry = (canonicalAlleleId) =>
-  fetch(`https://reg.clinicalgenome.org/allele/${canonicalAlleleId.toUpperCase()}`)
+const fetchVariantIdFromClingenRegistry = (canonicalAlleleId, resolveLiftover = true) => {
+  const promise = fetch(`https://reg.clinicalgenome.org/allele/${canonicalAlleleId.toUpperCase()}`)
     .then(null, () => {
       throw new Error("Error querying ClinGen Allele Registry");
     })
@@ -61,7 +69,44 @@ const fetchVariantIdFromClingenRegistry = (canonicalAlleleId) =>
       return record[0].id;
     });
 
-const ClingenAlleleIdForm = ({ onMatch }) => {
+  if (resolveLiftover) {
+    // ClinGen Alelle Registry only returns GRCh37 variant ids.
+    return promise.then((variantId) => {
+      const query = `
+      {
+        liftover(source_variant_id: "${variantId}", reference_genome: GRCh37) {
+          liftover {
+            variant_id
+          }
+        }
+      }`;
+
+      return fetch(`https://gnomad.broadinstitute.org/api?query=${query}`)
+        .then(null, () => {
+          throw new Error("Error querying gnomAD API");
+        })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("Error querying gnomAD API");
+        })
+        .then((response) => {
+          const liftoverVariantId = response.data?.liftover[0]?.liftover?.variant_id;
+          if (!variantId) {
+            throw new Error(`No liftover variant ID found for '${canonicalAlleleId}/${variantId}'`);
+          }
+
+          // Callback expects anything with the 'includes' method
+          return [variantId, liftoverVariantId].join("|");
+        });
+    });
+  }
+
+  return promise;
+};
+
+const ClingenAlleleIdForm = ({ onMatch, resolveLiftover }) => {
   const [caId, setCaId] = useState("");
   const [errorMessage, setErrorMessage] = useState(null);
   const [isFetching, setIsFetching] = useState(false);
@@ -76,7 +121,7 @@ const ClingenAlleleIdForm = ({ onMatch }) => {
       onSubmit={() => {
         setIsFetching(true);
         setErrorMessage(null);
-        fetchVariantIdFromClingenRegistry(caId).then(
+        fetchVariantIdFromClingenRegistry(caId, resolveLiftover).then(
           (variantId) => {
             setIsFetching(false);
             onMatch(variantId);
@@ -88,6 +133,10 @@ const ClingenAlleleIdForm = ({ onMatch }) => {
         );
       }}
     >
+      <p>
+        <b>Note:</b> ClinGen Allele Registry can only match against GRCh37 variant ids. Variants
+        will be omitted from the search results if a liftover variant id cannot be found.
+      </p>
       <Form.Input
         disabled={isFetching}
         error={showError}
@@ -106,15 +155,29 @@ const ClingenAlleleIdForm = ({ onMatch }) => {
       <Button disabled={!caId || !isValid || isFetching} type="submit">
         Search
       </Button>
+      <Button
+        type="reset"
+        onClick={() => {
+          setCaId("");
+          onMatch(null);
+        }}
+      >
+        Clear
+      </Button>
     </Form>
   );
 };
 
 ClingenAlleleIdForm.propTypes = {
   onMatch: PropTypes.func.isRequired,
+  resolveLiftover: PropTypes.bool,
 };
 
-const VariantSearch = withRouter(({ history }) => {
+ClingenAlleleIdForm.defaultProps = {
+  resolveLiftover: true,
+};
+
+const VariantSearch = ({ onSearch }) => {
   const [searchType, setSearchType] = useState("VariantID");
   return (
     <Segment attached>
@@ -129,26 +192,32 @@ const VariantSearch = withRouter(({ history }) => {
             label="Variant ID"
             value="VariantID"
             checked={searchType === "VariantID"}
-            onChange={(e, { value }) => setSearchType(value)}
+            onChange={(e, { value }) => {
+              setSearchType(value);
+              onSearch(null);
+            }}
           />
           <Form.Radio
             id="variant-search-type-caid"
             label="ClinGen Canonical Allele ID"
             value="CAID"
             checked={searchType === "CAID"}
-            onChange={(e, { value }) => setSearchType(value)}
+            onChange={(e, { value }) => {
+              setSearchType(value);
+              onSearch(null);
+            }}
           />
         </Form.Group>
       </Form>
 
-      {searchType === "VariantID" && (
-        <VariantIdForm onSubmit={(variantId) => history.push(`/variant/${variantId}/`)} />
-      )}
-      {searchType === "CAID" && (
-        <ClingenAlleleIdForm onMatch={(variantId) => history.push(`/variant/${variantId}/`)} />
-      )}
+      {searchType === "VariantID" && <VariantIdForm onSubmit={onSearch} />}
+      {searchType === "CAID" && <ClingenAlleleIdForm onMatch={onSearch} resolveLiftover />}
     </Segment>
   );
-});
+};
+
+VariantSearch.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+};
 
 export default VariantSearch;
