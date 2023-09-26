913a6ae5a56a440be4775804d55998bdd2c3dbcc
Daniel Esposito <7043686+daniaki@users.noreply.github.com>
2023-09-26 18:22:12 +1000

    Verdict validation logic

--- assets/utilities/CurationFlagRules.js
+++ assets/utilities/CurationFlagRules.js
@@ -1,52 +1,67 @@
 import verdicts from "../constants/verdicts";
+import { FLAGS } from "../constants/flags";
 
-const CurationFlagRules = (flags) => {
-  const verdictFlags = Object.fromEntries(verdicts.map((verdict) => [verdict, false]));
-  const trueFlags = Object.entries(flags).filter(
-    ([flag, val]) =>
-      (flag !== "custom_flags" && val && flag !== "verdict") ||
-      (flag === "custom_flags" && Object.keys(val).length)
-  );
-  if (!trueFlags.length) {
-    // if no flags selected, only lof can be chosen as verdict
-    verdictFlags["lof"] = true;
-    return verdictFlags;
+export class CurationResultVerdictValidator {
+  constructor() {
+    this.flags = FLAGS.map((f) => f.key);
   }
-  if (flags["flag_flow_chart_overridden"]) {
-    //overrides rules
-    verdictFlags["lof"] = true;
-    verdictFlags["likely_lof"] = true;
-    verdictFlags["uncertain"] = true;
-    verdictFlags["likely_not_lof"] = true;
-    verdictFlags["not_lof"] = true;
-    return verdictFlags;
-  }
-  if (flags["flag_no_read_data"]) {
-    verdictFlags["uncertain"] = true;
-    return verdictFlags;
-  }
-  if (flags["flag_reference_error"]) {
-    verdictFlags["not_lof"] = true;
-    return verdictFlags;
-  }
-  if (
-    flags["flag_mapping_error"] ||
-    flags["flag_genotype_error"] ||
-    flags["flag_inconsequential_transcript"] ||
-    flags["flag_rescue"]
-  ) {
-    verdictFlags["uncertain"] = true;
-    verdictFlags["likely_not_lof"] = true;
-    verdictFlags["not_lof"] = true;
-    return verdictFlags;
-  }
-  if (trueFlags.some(([flag, value]) => flag !== "custom_flags")) {
-    verdictFlags["lof"] = true;
-    verdictFlags["likely_lof"] = true;
-    verdictFlags["uncertain"] = true;
-    return verdictFlags;
-  }
-  return verdictFlags;
-};
 
-export default CurationFlagRules;
+  isValid(result) {
+    const { verdict } = result;
+    if (!verdict) return true;
+
+    return this.getValidVerdicts(result).includes(verdict);
+  }
+
+  verdictIsAllowed(result, verdict) {
+    return this.getValidVerdicts(result).includes(verdict);
+  }
+
+  getValidVerdicts(result) {
+    const flags = Object.fromEntries(
+      Object.entries(result).filter(([key]) => this.flags.includes(key))
+    );
+
+    // Note: These don't include custom flags.
+    const anyFlagChecked = Object.values(flags).some((v) => v);
+
+    if (!anyFlagChecked) {
+      // if no flags selected, only lof can be chosen as verdict
+      return ["lof"];
+    }
+
+    if (flags.flag_flow_chart_overridden) {
+      // overrides rules, allow all.
+      return [...verdicts];
+    }
+
+    if (flags.flag_no_read_data) {
+      return ["uncertain"];
+    }
+
+    if (flags.flag_reference_error) {
+      return ["not_lof"];
+    }
+
+    if (
+      flags.flag_mapping_error ||
+      flags.flag_genotyping_error ||
+      flags.flag_inconsequential_transcript ||
+      flags.flag_rescue
+    ) {
+      return ["uncertain", "likely_not_lof", "not_lof"];
+    }
+
+    // If at least one flag is selected (that's not [mapping_error, genotype_error,
+    // inconsequential_transcript, rescue, no_read_data, reference_error, flow_Chart_overridden,
+    // any custom_flag]) then only 'LoF', 'likely LoF' and 'Uncertain' can be selected
+    if (anyFlagChecked) {
+      return ["lof", "likely_lof", "uncertain"];
+    }
+
+    // No rules are satisfied, no verdicts are allowed.
+    return [];
+  }
+}
+
+export default CurationResultVerdictValidator;
